03056c02f2ea985d4e31103c74e03474bdac0401
nandrease <neeme.andreas.eller@gmail.com>
2020-03-20 22:28:21 +0200

    Search result and word highlighting

--- src/app/wikipedia.service.ts
+++ src/app/wikipedia.service.ts
@@ -1,13 +1,24 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class WikipediaService {
-
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
   search(term: string) {
-    return `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${term}&utf8=&format=json`
+    return this.http.get("https://en.wikipedia.org/w/api.php", {
+      params: {
+        action: "query",
+        format: "json",
+        list: "search",
+        utf8: "1",
+        srsearch: term,
+        origin: "*"
+      }
+    });
+    // const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${term}&utf8=&format=json`;
+    // return this.http.get(url);
   }
 }
